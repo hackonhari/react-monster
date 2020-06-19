c0f61875a9ad5c3489547372027170137fc8c290
hackonhari <hackonhari@gmail.com>
2020-06-19 14:51:32 +0530

    initial commit

---
+++ build/precache-manifest.5cee7466720a968f095c10780e483f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086e4f9c338f6f4e23b05931e8ee4b38",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "e1a92a219b5e29b27aec",
+    "url": "/react-monster/static/css/main.fd031f71.chunk.css"
+  },
+  {
+    "revision": "f98611df157c4b4011cb",
+    "url": "/react-monster/static/js/2.4766997f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster/static/js/2.4766997f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a92a219b5e29b27aec",
+    "url": "/react-monster/static/js/main.8057f9b8.chunk.js"
+  },
+  {
+    "revision": "5659a3a806bcf1c2c783",
+    "url": "/react-monster/static/js/runtime-main.6da7e892.js"
+  }
+]);
